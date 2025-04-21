8dec1ec6ba911c011b062672cdea37ed20292f74
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-04-21 16:03:39 +0100

    6 - Routes & Parameters (#63)
    exercises for heads up

--- lib/heads_up_web/controllers/tip_controller.ex
+++ lib/heads_up_web/controllers/tip_controller.ex
@@ -2,10 +2,13 @@ defmodule HeadsUpWeb.TipController do
   use HeadsUpWeb, :controller
   alias HeadsUp.Tips
 
+  def show(conn, %{"id" => id}) do
+    tip = Tips.get_tip(id)
+    render(conn, :show, tip: tip)
+  end
+
   def index(conn, _params) do
-
     tips = Tips.list_tips()
-
     render(conn, :index, tips: tips)
   end
 
--- lib/heads_up_web/controllers/tip_html.ex
+++ lib/heads_up_web/controllers/tip_html.ex
@@ -3,4 +3,14 @@ defmodule HeadsUpWeb.TipHTML do
 
   embed_templates("tips_html/*")
 
+  def show(assigns) do
+    ~H"""
+    <div class="tips">
+      <h1>You Like a Tip, <%= @answer %>?</h1>
+      <p>
+        <%= @tip.text %>
+      </p>
+    </div>
+  """
+  end
 end
