62db55b4fe4e5e42bc06e7c8266ef77fd1499eb7
treejamie <me@treejamie.com>
2025-04-24 18:07:11 +0100

    And that's 19 - LiveView navigation. Lovely

--- lib/heads_up_web/live/incident_live/index.ex
+++ lib/heads_up_web/live/incident_live/index.ex
@@ -28,17 +28,19 @@ defmodule HeadsUpWeb.IncidentsLive.Index do
   attr :incident, HeadsUp.Incident, required: true
   def incident_card(assigns) do
     ~H"""
-    <div class="card">
-      <img src={@incident.image_path} />
-      <h2>{@incident.name}</h2>
-      <div class="details">
-        <.badge status={@incident.status} />
-        <div class="priority">
-          {@incident.priority}
+      <.link navigate={~p"/incidents/#{@incident}"}>
+      <div class="card">
+        <img src={@incident.image_path} />
+        <h2>{@incident.name}</h2>
+        <div class="details">
+          <.badge status={@incident.status} />
+          <div class="priority">
+            {@incident.priority}
+          </div>
         </div>
       </div>
-    </div>
-  """
+    </.link>
+    """
 
   end
 
--- lib/heads_up_web/live/incident_live/show.ex
+++ lib/heads_up_web/live/incident_live/show.ex
@@ -18,29 +18,29 @@ defmodule HeadsUpWeb.IncidentsLive.Show do
 
   def render(assigns) do
     ~H"""
-<div class="incident-show">
-  <div class="incident">
-    <img src={@incident.image_path} />
-    <section>
-      <.badge status={@incident.status} />
-      <header>
-        <h2>{@incident.name}</h2>
-        <div class="priority">
-         {@incident.priority}
-        </div>
-      </header>
-      <div class="description">
-      {@incident.description}
+    <div class="incident-show">
+      <div class="incident">
+        <img src={@incident.image_path} />
+        <section>
+          <.badge status={@incident.status} />
+          <header>
+            <h2>{@incident.name}</h2>
+            <div class="priority">
+            {@incident.priority}
+            </div>
+          </header>
+          <div class="description">
+          {@incident.description}
+          </div>
+        </section>
       </div>
-    </section>
-  </div>
-  <div class="activity">
-    <div class="left"></div>
-    <div class="right">
-      <.urgent_incidents incidents={@urgent_incidents} />
+      <div class="activity">
+        <div class="left"></div>
+        <div class="right">
+          <.urgent_incidents incidents={@urgent_incidents} />
+        </div>
+      </div>
     </div>
-  </div>
-</div>
     """
   end
 
@@ -49,10 +49,12 @@ defmodule HeadsUpWeb.IncidentsLive.Show do
     <section>
       <h4>Urgent Incidents</h4>
       <ul class="incidents">
-        <li :for={incident <- @incidents}>
-          <img src={incident.image_path}>
-          {incident.name}
-        </li>
+          <li :for={incident <- @incidents}>
+          <.link navigate={~p"/incidents/#{incident}"}>
+              <img src={incident.image_path}>
+              {incident.name}
+            </.link>
+          </li>
       </ul>
     </section>
     """
