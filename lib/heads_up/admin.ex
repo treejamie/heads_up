fa2ee0c6afc155606d184faba88846972a25f7de
treejamie <me@treejamie.com>
2025-04-30 09:50:55 +0100

    32 is done, but not  sending this for PR until the changesets are in place

--- lib/heads_up/admin.ex
+++ lib/heads_up/admin.ex
@@ -9,4 +9,15 @@ defmodule HeadsUp.Admin do
     |> order_by(desc: :inserted_at)
     |> Repo.all()
   end
+
+  def create_incident(attrs \\ %{} ) do
+   %Incident{
+    name: attrs["name"],
+    priority: attrs["priority"] |> String.to_integer(),
+    status: attrs["status"] |> String.to_existing_atom(),
+    description: attrs["description"],
+    image_path: attrs["image_path"],
+   }
+   |> Repo.insert!()
+  end
 end
--- lib/heads_up_web/live/admin_incident_live/form.ex
+++ lib/heads_up_web/live/admin_incident_live/form.ex
@@ -1,5 +1,6 @@
 defmodule HeadsUpWeb.AdminIncidentLive.Form do
   use HeadsUpWeb, :live_view
+  alias HeadsUp.Admin
 
   def mount(_params, _session, socket) do
     socket =
@@ -10,38 +11,44 @@ defmodule HeadsUpWeb.AdminIncidentLive.Form do
     {:ok, socket}
   end
 
+  def handle_event("save", %{"incident" => incident_params}, socket) do
+    Admin.create_incident(incident_params)
+
+    socket = push_navigate(socket, to: ~p"/admin/incidents")
+    {:noreply, socket}
+  end
+
   def render(assigns) do
     ~H"""
     <.header>
       {@page_title}
     </.header>
 
-    <.simple_form for={@form} id="incident-form">
+    <.simple_form for={@form} id="incident-form" phx-submit="save">
+      <.input field={@form["name"]} label="Name" />
 
-      <.input field={@form["name"]} label="Name"/>
+      <.input field={@form["description"]} type="textarea" label="Description" />
 
-      <.input field={@form["description"]} type="textarea" label="Description"/>
-
-      <.input field={@form["priority"]} type="number" label="Priority"/>
+      <.input field={@form["priority"]} type="number" label="Priority" />
 
       <.input
-          field={@form[:status]}
-          type="select"
-          label="Status"
-          prompt="Choose a status"
-          options={[:pending, :resolved, :canceled]}
-        />
+        field={@form[:status]}
+        type="select"
+        label="Status"
+        prompt="Choose a status"
+        options={[:pending, :resolved, :canceled]}
+      />
 
-      <.input field={@form["image_path"]} label="Image"/>
+      <.input field={@form["image_path"]} label="Image" />
 
       <:actions>
-        <.back navigate={~p"/admin/incidents"}> Back </.back>
+        <.button phx-disable-with="Saving...">Save Incident</.button>
       </:actions>
 
+      <:actions>
+        <.back navigate={~p"/admin/incidents"}>Back</.back>
+      </:actions>
     </.simple_form>
     """
-
   end
-
-
 end
