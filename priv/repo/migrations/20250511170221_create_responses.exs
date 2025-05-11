8d8976b17fd115dcd705a218348a4babb84445b5
Jamie Curle <1866+treejamie@users.noreply.github.com>
2025-05-11 18:16:14 +0100

    52 - Ecto Many to Many (#100)
    Many to many wiring is now done, next one is the form

---
+++ priv/repo/migrations/20250511170221_create_responses.exs
@@ -0,0 +1,19 @@
+defmodule HeadsUp.Repo.Migrations.CreateResponses do
+  use Ecto.Migration
+
+  def change do
+    create table(:responses) do
+      add :note, :text
+      add :status, :string
+      add :incident_id, references(:incidents, on_delete: :delete_all), null: false
+      add :user_id, references(:users, on_delete: :delete_all), null: false
+
+      timestamps(type: :utc_datetime)
+
+
+    end
+
+    create index(:responses, [:incident_id])
+    create index(:responses, [:user_id])
+  end
+end
