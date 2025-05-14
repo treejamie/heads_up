46c8e1cb0d0dfff6327a06a5a07a5becd2b0705a
treejamie <me@treejamie.com>
2025-05-14 12:01:13 +0100

    We are now completed. I have completed a whistlestop tour of Phoenix.

--- lib/heads_up_web/channels/presence.ex
+++ lib/heads_up_web/channels/presence.ex
@@ -8,4 +8,54 @@ defmodule HeadsUpWeb.Presence do
   use Phoenix.Presence,
     otp_app: :heads_up,
     pubsub_server: HeadsUp.PubSub
+
+  def init(_opts) do
+    {:ok, %{}}
+  end
+
+  def subscribe(id) do
+    Phoenix.PubSub.subscribe(HeadsUp.PubSub, "updates:" <> topic(id))
+  end
+
+  defp topic(incident_id) do
+    "incident_onlookers:#{incident_id}"
+  end
+
+  def list_users(id) do
+    list(topic(id))
+    |> Enum.map(fn {username, %{metas: metas}} ->
+      %{id: username, metas: metas}
+    end)
+  end
+
+  def track(id, current_user) do
+    track(self(), topic(id), current_user.username, %{
+      online_at: System.system_time(:second)
+    })
+  end
+
+  def handle_metas(topic, %{joins: joins, leaves: leaves}, presences, state) do
+    # the joiners
+    for {username, _presence} <- joins do
+      presence = %{id: username, metas: Map.fetch!(presences, username)}
+      msg = {:user_joined, presence}
+      Phoenix.PubSub.local_broadcast(HeadsUp.PubSub, "updates:" <> topic, msg)
+    end
+
+    # the leavers
+    for {username, _presence} <- leaves do
+      metas =
+        case Map.fetch(presences, username) do
+          {:ok, presence_metas} -> presence_metas
+          :error -> []
+        end
+
+      presence = %{id: username, metas: metas}
+      msg = {:user_left, presence}
+
+      Phoenix.PubSub.local_broadcast(HeadsUp.PubSub, "updates:" <> topic, msg)
+    end
+
+    {:ok, state}
+  end
 end
